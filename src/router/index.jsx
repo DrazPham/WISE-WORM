240aa080f085ae6a4bba209d35815db88fe168a3
Kien H <kienhh1007@gmail.com>
2024-09-02 19:42:09 +0700

    Clean up

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,20 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import LayoutEight from "components/layout/LayoutEight.jsx";
 import LayoutOne from "components/layout/LayoutOne.jsx";
 import Layout from "components/layout/index.jsx";
 import ErrorPage from "src/error-page";
 import AboutUs from "page/about/AboutUs";
 import ContactUs from "page/contact/ContactUs";
-import Reset from "page/auth/ResetPassword.jsx";
-import SignIn from "page/auth/SignIn";
-import SignUp from "page/auth/SignUp";
 import BlogGridPage from "page/blog/BlogGridPage.jsx";
 import SingleBlogPage from "page/blog/SingleBlog.jsx";
 import Home from "page/home/Home.jsx";
 import Moments from "page/moments/Moments";
 import MomentsDetail from "page/moments-detail/SingleMoment"
 import SingleTeam from "page/team-detail/SingleTeam.jsx";
-import CommingSoon from "page/utility/CommingSoon.jsx";
 import Faq from "page/utility/Faq.jsx";
 import TestimonialPage from "page/utility/Testimonial.jsx";
 
@@ -71,28 +66,6 @@ export const router = createBrowserRouter([
 					{
 						path: "*",
 						element: <ErrorPage />,
-					},
-				],
-			},
-			{
-				path: "/",
-				element: <LayoutEight />,
-				children: [
-					{
-						path: "/coming-soon",
-						element: <CommingSoon />,
-					},
-					{
-						path: "/reset-password",
-						element: <Reset />,
-					},
-					{
-						path: "/sign-up",
-						element: <SignUp />,
-					},
-					{
-						path: "/sign-in",
-						element: <SignIn />,
 					},
 				],
 			},
